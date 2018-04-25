1a86a9a6094aae2fe168d28b1edb070ceb84ed8c
Samarth Dhingra <samarthd927@gmail.com>
2018-04-25 12:02:14 +0530

    Added CRUD Abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+	before_action :find_posts,only: [:show ,:edit, :destroy,:update]
+	def index
+	end
+
+	def new 
+		@post = Post.new
+	end
+
+	def create 
+		@post = Post.new post_params  
+		
+		if @post.save 
+			redirect_to @post , notice:"Article saved"
+		else
+			render 'new',notice:"Not Saved" 
+		end 
+	end 
+
+	def show 
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params 
+			redirect_to @post, notice:"Sved successfully" 
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@post.destroy 
+		redirect_to posts_path 
+	end
+
+
+
+	private 
+
+
+
+	def post_params
+		params.require(:post).permit(:title,:content)
+	end
+
+	def find_posts
+		@post = Post.find(params[:id])
+	end
+
+end
+
+
