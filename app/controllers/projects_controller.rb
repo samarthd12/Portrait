61a39e9dee37de693ee74ff966110c07a4a0c8ff
Samarth Dhingra <samarthd927@gmail.com>
2018-04-26 20:10:18 +0530

    Setup friendly-id gem for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,12 +28,12 @@ class ProjectsController < ApplicationController
 	private 
 
 	def find_projects
-		@project = Project.find(params[:id])
+		@project = Project.friendly.find(params[:id])
 	end
 
 
 	def project_params
-		params.require(:project).permit(:title,:description,:link)
+		params.require(:project).permit(:title,:description,:link,:slug)
 	end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426111849) do
+ActiveRecord::Schema.define(version: 20180426143345) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20180426111849) do
     t.string   "link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
+
+  add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true
 
 end
