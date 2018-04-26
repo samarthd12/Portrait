f69f688704366a481c6cb097ae97181a4c148806
Samarth Dhingra <samarthd927@gmail.com>
2018-04-26 21:19:24 +0530

    Added CRUD for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,6 +24,23 @@ class ProjectsController < ApplicationController
 
 	end
 
+	def edit
+	end 
+
+	def update
+		if @project.update project_params 
+			redirect_to @project,notice: "Success"
+		else
+			render 'edit' 
+		end
+
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path 
+	end
+
 	
 	private 
 
